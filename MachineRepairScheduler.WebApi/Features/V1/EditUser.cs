5c9fbd506c577b5eb3229a52b11a42769c00d7ab
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2019-12-01 11:41:13 +0100

    add sysAdmin role to EDIT

--- MachineRepairScheduler.WebApi/Features/V1/EditUser.cs
+++ MachineRepairScheduler.WebApi/Features/V1/EditUser.cs
@@ -92,6 +92,7 @@ namespace MachineRepairScheduler.WebApi.Features.V1
 
         public enum Role
         {
+            SysAdmin = 0,
             Employee = 1,
             Technician = 2,
             PlanningManager = 3
@@ -101,7 +102,7 @@ namespace MachineRepairScheduler.WebApi.Features.V1
         {
             public CommandValidator()
             {
-                RuleFor(x => x.Role).Must(x => x > 0 && (int)x < 4).WithMessage("Invalid role.");
+                RuleFor(x => x.Role).Must(x => x > 0 && (int)x <= 4).WithMessage("Invalid role.");
                 RuleFor(x => x.FirstName).Must(x => x.Length > 1 && x.Length < 30).WithMessage("Must have minimum of 2 chars and maximum of 29 chars.");
                 RuleFor(x => x.LastName).Must(x => x.Length > 1 && x.Length < 30).WithMessage("Must have minimum of 2 chars and maximum of 29 chars.");
                 RuleFor(x => x.PhoneNumber).Must(IsEmptyOrPhoneNumber).WithMessage("Invalid phone number.");
