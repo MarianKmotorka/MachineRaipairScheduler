bf407134e57f390ec27d43f89560a22e9c73e598
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2019-12-13 20:42:55 +0100

    Clumsy global exception handler for UI

--- MachineRepairScheduler.Desktop/Program.cs
+++ MachineRepairScheduler.Desktop/Program.cs
@@ -1,17 +1,25 @@
 ﻿using MachineRepairScheduler.Desktop.Forms;
 using System;
+using System.Net.Http;
 using System.Windows.Forms;
 
 namespace MachineRepairScheduler.Desktop
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                if (((Exception)e.ExceptionObject).InnerException is HttpRequestException)
+                    MessageBox.Show("Could not connect to the server. Application will close.", "Connection problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    MessageBox.Show($"Following error has occured: {((Exception)e.ExceptionObject).Message}. Application will close.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Environment.Exit(-1);
+            };
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BackgroundForm());
