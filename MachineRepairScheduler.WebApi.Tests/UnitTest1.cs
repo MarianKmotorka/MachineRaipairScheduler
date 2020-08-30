f9f94d806666601456b2fa4271388788e165cefc
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-08-30 14:35:08 +0200

    Dummy tests

---
+++ MachineRepairScheduler.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace MachineRepairScheduler.WebApi.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void GivenAdamHumpliar_WhenTestingForHumpliarstvo_AdamIsPositive()
+        {
+            var isAdamHumpliar = true;
+            Assert.True(isAdamHumpliar);
+        }
+
+        [Test]
+        public void GivenAdamHumpliar_WhenTestingIQ_AdamFails()
+        {
+            var iq = 53;
+            Assert.False(iq > 100);
+        }
+    }
+}
